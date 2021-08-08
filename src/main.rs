bdbbb4048c29bb36dd7e1ca17d0cf68d478cbcb2
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-08-08 22:12:09 +0530

    Prepare code for other algorithms
    This commit allows for using other page replacement algorithms with the same main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod fifo_page;
 mod input_rs;
 #[path = "page.rs"]
 mod page_rs;
-use fifo_page::page_replacement::PageReplacement;
+#[path = "pagereplacement.rs"]
+mod page_replacement;
 
 fn main() {
     let mut demopage = page_rs::Page {
@@ -14,18 +15,36 @@ fn main() {
     println!("Number of frames: ?");
     let frames: u8 = input_rs::input_u8();
     let pagestring = demopage.get_page_string();
-    let mut pagereplacement = PageReplacement {
+    let mut pagereplacement = page_replacement::PageReplacement {
         ..Default::default()
     };
     pagereplacement = pagereplacement.add_page_string(pagestring, frames);
-    let fifopages = fifo_page::fifo(pagereplacement);
-    for i in 0..fifopages.status.len() {
-        for j in 0..fifopages.frame[i].len() {
-            println!("{}", fifopages.frame[i][j]);
+    let processedpages = fifo(pagereplacement);
+    for i in 0..processedpages.status.len() {
+        for j in 0..processedpages.frame[i].len() {
+            println!("{}", processedpages.frame[i][j]);
         }
-        println!("{}", fifopages.status[i]);
+        println!("{}", processedpages.status[i]);
     }
     println!("Completed.");
     println!("At the moment, only FIFO replacement is supported. ");
     println!("Support for more algorithms coming soon...");
+}
+
+fn fifo(pagereplacement: page_replacement::PageReplacement) -> page_replacement::PageReplacement {
+    let mut prefifopages = fifo_page::page_replacement::PageReplacement {
+        page_reference: Vec::<String>::new(),
+        frame: pagereplacement.frame,
+        status: pagereplacement.status,
+        framecount: 0,
+    };
+    prefifopages = prefifopages.add_page_string(pagereplacement.page_reference, pagereplacement.framecount);
+    let fifopages = fifo_page::fifo(prefifopages);
+    let processedpages = page_replacement::PageReplacement {
+        page_reference: fifopages.page_reference,
+        frame: fifopages.frame,
+        status: fifopages.status,
+        framecount: fifopages.framecount,
+    };
+    return processedpages; 
 }
