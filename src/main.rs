ab5b68d8c028f87a62743a900607b9aed0df89d7
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-08-09 00:08:52 +0530

    Placeholders for LRU and optimal algorithms
    NOTE: Both those algorithms are not yet implemented. For the time being, only fifo.rs is giving correct output.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-#[path = "fifo.rs"]
-mod fifo_page;
 #[path = "input.rs"]
 mod input_rs;
-#[path = "page.rs"]
-mod page_rs;
 #[path = "pagereplacement.rs"]
 mod page_replacement;
+#[path = "page.rs"]
+mod page_rs;
+// All page replacement algorithms imports
+#[path = "fifo.rs"]
+mod fifo_page;
+#[path = "lru.rs"]
+mod lru_page;
+#[path = "optimal.rs"]
+mod optimal_page;
 
 fn main() {
     let mut demopage = page_rs::Page {
@@ -19,7 +24,7 @@ fn main() {
         ..Default::default()
     };
     pagereplacement = pagereplacement.add_page_string(pagestring, frames);
-    let processedpages = fifo(pagereplacement);
+    let processedpages = choose_algo(pagereplacement);
     for i in 0..processedpages.status.len() {
         for j in 0..processedpages.frame[i].len() {
             println!("{}", processedpages.frame[i][j]);
@@ -27,9 +32,25 @@ fn main() {
         println!("{}", processedpages.status[i]);
     }
     println!("Completed.");
-    println!("At the moment, only FIFO replacement is supported. ");
-    println!("Support for more algorithms coming soon...");
 }
+
+// Choosing a page replacement algorithm
+
+fn choose_algo(p: page_replacement::PageReplacement) -> page_replacement::PageReplacement {
+    println!("Choose a page replacement algorithm");
+    println!("0: First-In-First-Out");
+    println!("1: Least Recently Used");
+    println!("2: Optimal");
+    let choice = input_rs::input_u8();
+    match choice {
+        0 => fifo(p),
+        1 => lru(p),
+        2 => optimal(p),
+        3..=255 => panic!("Invalid choice"),
+    }
+}
+
+// Add all these page replacement algorithm setup here.
 
 fn fifo(pagereplacement: page_replacement::PageReplacement) -> page_replacement::PageReplacement {
     let mut prefifopages = fifo_page::page_replacement::PageReplacement {
@@ -38,7 +59,8 @@ fn fifo(pagereplacement: page_replacement::PageReplacement) -> page_replacement:
         status: pagereplacement.status,
         framecount: 0,
     };
-    prefifopages = prefifopages.add_page_string(pagereplacement.page_reference, pagereplacement.framecount);
+    prefifopages =
+        prefifopages.add_page_string(pagereplacement.page_reference, pagereplacement.framecount);
     let fifopages = fifo_page::fifo(prefifopages);
     let processedpages = page_replacement::PageReplacement {
         page_reference: fifopages.page_reference,
@@ -46,5 +68,45 @@ fn fifo(pagereplacement: page_replacement::PageReplacement) -> page_replacement:
         status: fifopages.status,
         framecount: fifopages.framecount,
     };
-    return processedpages; 
+    return processedpages;
+}
+
+fn optimal(
+    pagereplacement: page_replacement::PageReplacement,
+) -> page_replacement::PageReplacement {
+    let mut preoptimalpages = optimal_page::page_replacement::PageReplacement {
+        page_reference: Vec::<String>::new(),
+        frame: pagereplacement.frame,
+        status: pagereplacement.status,
+        framecount: 0,
+    };
+    preoptimalpages =
+        preoptimalpages.add_page_string(pagereplacement.page_reference, pagereplacement.framecount);
+    let optimalpages = optimal_page::optimal(preoptimalpages);
+    let processedpages = page_replacement::PageReplacement {
+        page_reference: optimalpages.page_reference,
+        frame: optimalpages.frame,
+        status: optimalpages.status,
+        framecount: optimalpages.framecount,
+    };
+    return processedpages;
+}
+
+fn lru(pagereplacement: page_replacement::PageReplacement) -> page_replacement::PageReplacement {
+    let mut prelrupages = lru_page::page_replacement::PageReplacement {
+        page_reference: Vec::<String>::new(),
+        frame: pagereplacement.frame,
+        status: pagereplacement.status,
+        framecount: 0,
+    };
+    prelrupages =
+        prelrupages.add_page_string(pagereplacement.page_reference, pagereplacement.framecount);
+    let lrupages = lru_page::lru(prelrupages);
+    let processedpages = page_replacement::PageReplacement {
+        page_reference: lrupages.page_reference,
+        frame: lrupages.frame,
+        status: lrupages.status,
+        framecount: lrupages.framecount,
+    };
+    return processedpages;
 }
---
+++ src/optimal.rs
@@ -0,0 +1,64 @@
+use std::collections::VecDeque;
+#[path = "hit.rs"]
+pub mod hit_rs;
+#[path = "pagereplacement.rs"]
+pub mod page_replacement;
+use page_replacement::PageReplacement;
+
+pub fn optimal(mut p: PageReplacement) -> PageReplacement {
+    println!("FIFO Page replacement demonstration\n");
+    // Logs all the pages used. Assuming each page to be represented as a string...
+    let mut log: VecDeque<String> = VecDeque::<String>::new();
+    // Traverse through each page in the page reference string
+    for i in 0..p.page_reference.len() - 1 {
+        // The previous frame
+        let mut prev: Vec<String> = Vec::<String>::new();
+        // The current frame
+        let mut current: Vec<String> = Vec::<String>::new();
+        // If not the first frame
+        if i > 0 {
+            // Copy the previous frame from the original frame to prev and current
+            for j in 0..p.frame[i - 1].len() {
+                let val = p.frame[i - 1].get(j);
+                let strval = String::from(val.unwrap_or(&String::new()));
+                prev.push(strval);
+                let strval = String::from(val.unwrap_or(&String::new()));
+                current.push(strval);
+                let strval = String::from(val.unwrap_or(&String::new()));
+                if !log.contains(&strval) {
+                    log.push_back(strval);
+                }
+            }
+            // If current does not have the page, it means page is not in memory
+            // Lookup for the page
+            if !current.contains(&p.page_reference[i]) {
+                if current.len() == p.framecount.into() {
+                    // The frame is filled. Replacement is necessary
+                    let replaced = log.pop_front().unwrap();
+                    for j in 0..current.len() {
+                        // Replace the page first logged into the memory
+                        if current[j] == replaced {
+                            let new_val = p.page_reference.get(i);
+                            current[j] = String::from(new_val.unwrap_or(&String::new()));
+                        }
+                    }
+                } else if current.len() < p.framecount.into() {
+                    // The frame is not filled. Add the page to the frame.
+                    let val = p.page_reference.get(i);
+                    let strval = String::from(val.unwrap_or(&String::new()));
+                    current.push(strval);
+                }
+            }
+        } else {
+            // First frame. Simply add the first page into memory.
+            let val = p.page_reference.get(0);
+            let strval = String::from(val.unwrap_or(&String::new()));
+            current.push(strval);
+        }
+        // Generate if miss (page fault, not in memory) or hit (page in memory)
+        p.status.push(hit_rs::is_hit(&current, &prev));
+        // Save the current frame
+        p.frame.push(current);
+    }
+    return p;
+}
