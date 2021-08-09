903ebc74257e228e1804e5361962f2c09b3d2201
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2021-08-09 12:13:40 +0530

    Use correct optimal page replacement algorithm

--- src/optimal.rs
+++ src/optimal.rs
@@ -6,9 +6,15 @@ pub mod page_replacement;
 use page_replacement::PageReplacement;
 
 pub fn optimal(mut p: PageReplacement) -> PageReplacement {
-    println!("FIFO Page replacement demonstration\n");
+    println!("Optimal Page replacement demonstration\n");
     // Logs all the pages used. Assuming each page to be represented as a string...
     let mut log: VecDeque<String> = VecDeque::<String>::new();
+    // Copy all pages into log
+    for i in 0..p.page_reference.len() - 1 {
+        log.push_back(String::from(
+            p.page_reference.get(i).unwrap_or(&String::new()),
+        ));
+    }
     // Traverse through each page in the page reference string
     for i in 0..p.page_reference.len() - 1 {
         // The previous frame
@@ -24,17 +30,35 @@ pub fn optimal(mut p: PageReplacement) -> PageReplacement {
                 prev.push(strval);
                 let strval = String::from(val.unwrap_or(&String::new()));
                 current.push(strval);
-                let strval = String::from(val.unwrap_or(&String::new()));
-                if !log.contains(&strval) {
-                    log.push_back(strval);
-                }
             }
             // If current does not have the page, it means page is not in memory
             // Lookup for the page
             if !current.contains(&p.page_reference[i]) {
                 if current.len() == p.framecount.into() {
                     // The frame is filled. Replacement is necessary
-                    let replaced = log.pop_front().unwrap();
+                    let mut replaced = String::new();
+                    let mut is_found: bool = true;
+                    let mut find_index: usize = 0;
+                    for j in 0..current.len() {
+                        let strval = String::from(current.get(j).unwrap_or(&String::new()));
+                        if !log.contains(&strval) && is_found {
+                            replaced = strval;
+                            is_found = false;
+                        }
+                    }
+                    if is_found {
+                        for j in 0..current.len() {
+                            let strval = String::from(current.get(j).unwrap_or(&String::new()));
+                            for k in 0..log.len() {
+                                let found_strval =
+                                    String::from(log.get(k).unwrap_or(&String::new()));
+                                if found_strval == strval && found_strval != String::new() {
+                                    find_index = k;
+                                }
+                            }
+                        }
+                        replaced = String::from(log.get(find_index).unwrap_or(&String::new()));
+                    }
                     for j in 0..current.len() {
                         // Replace the page first logged into the memory
                         if current[j] == replaced {
@@ -57,6 +81,8 @@ pub fn optimal(mut p: PageReplacement) -> PageReplacement {
         }
         // Generate if miss (page fault, not in memory) or hit (page in memory)
         p.status.push(hit_rs::is_hit(&current, &prev));
+        // Remove the used frame from the log
+        let _popped = log.pop_front();
         // Save the current frame
         p.frame.push(current);
     }
